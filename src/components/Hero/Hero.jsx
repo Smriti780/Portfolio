bdbb1c07214dc046163aca0e6fa5276d81cbcb08
Smriti <smrititiwari780@gmail.com>
2025-06-12 14:35:36 +0530

    commited

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h4 className={styles.title}>Hi, I'm Smriti Tiwari</h4>
+        <h1 className={styles.title}>Hi, I'm Smriti Tiwari</h1>
         <p className={styles.description}>
           I am a full‑stack web developer in my final year of B.Tech(CSE) —bringing ideas 
           to life using React and Node.js. Reach out to collaborate!
